b86adf2e6da5971e1a692ffef3b1b7a2151ea1d1
Doktor <doktorthehusky@gmail.com>
2018-09-03 06:10:48 -0400

    Remove container margin on 'featured' page

--- static/scripts/featured.js
+++ static/scripts/featured.js
@@ -1,6 +1,4 @@
-let el = document.querySelector('#featured');
-const masonry = new Masonry(el, {
-    itemSelector: '.item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
+let el = document.querySelector('#featured .container');
+new Masonry(el, {
+  itemSelector: '.item',
 });
