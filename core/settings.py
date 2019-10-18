c6d886be0e3f6911638af4c99c2e3cb0252c7fae
Doktor <doktorthehusky@gmail.com>
2019-10-17 23:41:31 -0400

    api: Install 'django-mptt'

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptt',
     'rest_framework',
     'storages',
     'photos',
