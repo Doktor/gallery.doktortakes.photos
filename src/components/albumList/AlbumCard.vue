ce4d28622b8c92fe94de26732571dc065dd5003d
Doktor <doktorthehusky@gmail.com>
2020-06-01 06:14:20 -0400

    ui [AlbumCard]: add skeleton screen layout

--- src/components/albumList/AlbumCard.vue
+++ src/components/albumList/AlbumCard.vue
@@ -1,49 +1,81 @@
 <template>
   <div
       class="album-wrapper"
-      :class="{'hidden': !this.isVisible}"
+      :class="{'hidden': !isSkeleton && !isVisible}"
   >
     <div
         class="album"
-        :class="classes"
+        :class="albumClasses"
     >
-      <router-link
-          :to="{name: route, params: {path: album.pathSplit}}"
+      <div
+          :is="isSkeleton ? 'div' : 'router-link'"
+          :to="isSkeleton ? null : {name: route, params: {path: album.pathSplit}}"
       >
-        <img
-            v-if="album.cover"
-            :src="thumbnail"
-            :title="album.name"
-            alt="Album cover"
-        >
-        <div v-else>
-          <img
-              :src="placeholder"
-              :title="album.name"
-              alt="Album cover placeholder"
+        <div>
+          <template v-if="!isSkeleton">
+            <!-- Album loaded -->
+            <img
+                v-if="album.cover"
+                :src="thumbnail"
+                :title="album.name"
+                alt="Album cover"
+            >
+            <!-- Album loaded, no cover image -->
+            <div v-else>
+              <img
+                  :src="placeholder"
+                  :title="album.name"
+                  alt="Album cover placeholder"
+              >
+              <div class="note album-no-cover-note">No cover</div>
+            </div>
+          </template>
+          <!-- Skeleton -->
+          <div
+              v-else
+              class="album-cover-placeholder"
+              :class="albumCoverSkeletonClasses"
           >
-          <div class="note album-no-cover-note">No cover</div>
-        </div>
+            <img
+                :src="placeholder"
+                alt="Album cover placeholder"
+                title="Loading..."
+            >
+            <div class="note album-no-cover-note">
+              <i class="fas fa-spin fa-spinner"></i>
+            </div>
+          </div>
 
-        <div class="album-title">
-          <div>{{ album.name }}</div>
+          <div class="album-title">
+            <div class="album-title-text loading">{{ album.name }}</div>
+          </div>
         </div>
-      </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import {staticFiles} from "../../store/index.js";
+  import {staticFiles} from "../../store";
 
 
   export default {
     computed: {
-      classes() {
+      albumClasses() {
         return {
           'album-hidden': this.album.access_level > 0,
           'album-no-cover': this.album.cover === null,
         }
+      },
+
+      albumCoverSkeletonClasses() {
+        return {
+          'loading': this.isSkeleton,
+        }
+      },
+
+      placeholder() {
+        return staticFiles.coverPlaceholder;
       },
 
       thumbnail() {
@@ -51,12 +83,6 @@
           ? this.album.cover.thumbnail
           : this.placeholder;
       },
-    },
-
-    data() {
-      return {
-        placeholder: staticFiles.coverPlaceholder,
-      }
     },
 
     props: {
@@ -69,9 +95,18 @@
         default: "album",
       },
 
+      index: {
+        type: Number,
+        required: true,
+      },
+
       isLoaded: {
         type: Boolean,
-        required: true,
+        default: false,
+      },
+      isSkeleton: {
+        type: Boolean,
+        default: false,
       },
       isVisible: {
         type: Boolean,
@@ -80,3 +115,17 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .album-title-text.loading {
+    height: 1.1rem;
+  }
+
+  .album-cover-placeholder {
+    position: relative;
+
+    &.loading {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+  }
+</style>
