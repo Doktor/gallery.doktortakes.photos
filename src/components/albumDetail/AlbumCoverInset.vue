517ac338d25d20d30507d129d9bed1658e9078a0
Doktor <doktorthehusky@gmail.com>
2020-06-03 05:58:34 -0400

    ui: Move all AlbumInfo elements into AlbumCoverInset

--- src/components/albumDetail/AlbumCoverInset.vue
+++ src/components/albumDetail/AlbumCoverInset.vue
@@ -1,62 +1,95 @@
 <template>
-  <div class="group-inset">
-    <div class="group-inset-row">
-      <h2 class="group-inset-text group-title">{{ album.name }}</h2>
+  <div class="group-inset-container">
+    <div class="group-inset-header">
+      <div class="group-inset-row">
+        <h2 class="group-inset-text group-title">{{ album.name }}</h2>
+      </div>
+
+      <div class="group-inset-text">
+        <span v-html="date"></span> &middot;
+        <span>{{ photos.length }} photo{{ photos.length|pluralize }}</span>
+      </div>
     </div>
 
-    <div class="group-inset-text">
-      <span v-if="location">{{ location }} &middot; </span>
-      <span v-html="date"></span> &middot;
-      <span>{{ photos.length }} photo{{ photos.length|pluralize }}</span>
-    </div>
+    <div class="group-inset-body">
+      <div v-if="location" class="group-inset-list-item">
+        <i title="Location" class="fas fa-fw fa-map-marker-alt"></i>
+        <span>{{ location }}</span>
+      </div>
 
-    <div v-if="album.parent || userIsStaff" class="group-inset-text">
-      <router-link
-          v-if="album.parent"
-          :to="{name: 'album', params: {path: album.parent.split('/')}}"
-      >
-        View parent album
-      </router-link>
+      <AlbumAccessInfo/>
 
-      <template v-if="userIsStaff">
-        <span v-if="album.parent">&middot;</span>
+      <div v-if="album.tags.length > 0" class="group-inset-list-item">
+        <i title="Tags" class="fas fa-fw fa-tags"></i>
+        <span>
+          <template v-for="(slug, index) in album.tags">
+            <router-link
+              class="tag"
+              :to="{name: 'tag', params: {slug: slug}}"
+              ><!--
+                -->#{{ slug }}<!--
+              --></router-link>
+            <span v-if="index !== album.tags.length - 1" v-html="nbsp"></span>
+          </template>
+        </span>
+      </div>
+
+      <div v-if="album.description"
+          class="group-inset-list-item group-description">
+        <i title="Description" class="fas fa-fw fa-book"></i>
+        <span v-html="album.description"></span>
+      </div>
+
+      <div v-if="album.parent || userIsStaff" class="group-inset-list-item">
+        <i title="Links" class="fas fa-fw fa-link"></i>
 
         <router-link
-          :to="{name: 'editAlbum', params: {path: album.path}}"
+            v-if="album.parent"
+            :to="{name: 'album', params: {path: album.parent.split('/')}}"
         >
-          Edit
+          View parent album
         </router-link>
 
-        &middot;
+        <template v-if="userIsStaff">
+          <span v-if="album.parent">&middot;</span>
 
-        <a :href="album.admin_url">Admin</a>
+          <router-link
+            :to="{name: 'editAlbum', params: {path: album.path}}"
+          >
+            Edit
+          </router-link>
 
-        &middot;
+          &middot;
 
-        <a
-          :href="urlProductionSite"
-          title="View album on production site"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Production</a>
+          <a :href="album.admin_url">Admin</a>
 
-        &middot;
+          &middot;
 
-        <a
-          :href="urlAlphaSite"
-          title="View album on alpha site"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Alpha</a>
-      </template>
+          <a
+            :href="urlProductionSite"
+            title="View album on production site"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Production</a>
+
+          &middot;
+
+          <a
+            :href="urlAlphaSite"
+            title="View album on alpha site"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Alpha</a>
+        </template>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import {domains} from "../../store";
   import {mapState} from 'vuex';
-
+  import {domains} from "../../store";
+  import AlbumAccessInfo from "./AlbumAccessInfo.vue";
 
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -75,21 +108,16 @@
 
 
   export default {
+    components: {
+      AlbumAccessInfo,
+    },
+
     computed: {
       ...mapState([
         'album',
         'photos',
         'user',
       ]),
-
-      date() {
-        let start = new Date(this.album.start);
-        let end = this.album.end === null ? null : new Date(this.album.end);
-
-        return end === null
-          ? formatDate(start)
-          : "{0} &ndash; {1}".format(formatDate(start), formatDate(end));
-      },
 
       location() {
         let place = this.album.place;
@@ -102,6 +130,9 @@
         return place || location || "";
       },
 
+      nbsp() {
+        return "&nbsp;";
+      },
 
       urlAlphaSite() {
         return new URL(this.album.url, domains.alpha).href;
@@ -113,6 +144,15 @@
 
       userIsStaff() {
         return this.user.status === 'staff' || this.user.status === 'superuser';
+      },
+
+      date() {
+        let start = new Date(this.album.start);
+        let end = this.album.end === null ? null : new Date(this.album.end);
+
+        return end === null
+          ? formatDate(start)
+          : "{0} &ndash; {1}".format(formatDate(start), formatDate(end));
       },
     },
 
