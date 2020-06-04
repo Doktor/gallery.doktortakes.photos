daefb9c28b7d06a9b691c6fa074fc9dfc54d21ae
Doktor <doktorthehusky@gmail.com>
2020-06-04 03:18:06 -0400

    ui: Create FixedWidthContainer component

---
+++ src/components/FixedWidthContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fixed-width">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container-fixed-width {
+  max-width: 1200px;
+  text-align: left;
+  margin: 0 auto;
+}
+</style>
