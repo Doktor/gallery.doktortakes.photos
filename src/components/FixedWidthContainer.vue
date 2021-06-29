f8e85ae49bc99601e1740f16d0cfdff77b29fcff
Doktor <doktorthehusky@gmail.com>
2021-06-29 04:04:46 -0400

    ui [FixedWidthContainer]: change default width to 1000px and add width prop

--- src/components/FixedWidthContainer.vue
+++ src/components/FixedWidthContainer.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="container-fixed-width">
+  <div class="container-fixed-width" :style="styles">
     <slot></slot>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 1000,
+    },
+  },
+
+  computed: {
+    styles() {
+      return {
+        "max-width": `${this.width}px`,
+      }
+    },
+  },
+}
+</script>
+
 <style scoped>
 .container-fixed-width {
-  max-width: 1200px;
   text-align: left;
   margin: 0 auto;
 }
