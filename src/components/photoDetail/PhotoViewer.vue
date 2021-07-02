64a22eefd6f75184d3473ffc479ba177d824023e
Doktor <doktorthehusky@gmail.com>
2021-07-02 01:11:53 -0400

    #37 [PhotoViewer]: implement image panning
    Closes #37.

--- src/components/photoDetail/PhotoViewer.vue
+++ src/components/photoDetail/PhotoViewer.vue
@@ -5,7 +5,10 @@
       <img
         class="photo-image"
         ref="image"
-        @click="navigate"
+        @mousedown.prevent="mouseDown"
+        @mousemove.prevent="mouseMove"
+        @mouseup.prevent="mouseUp"
+        @mouseleave.prevent="mouseLeave"
         :src="imageUrl"
         :style="imageStyles"
         alt=""
@@ -55,6 +58,17 @@
         transitionProperties: ['transform', 'left', 'top'],
         transitionTime: 0.6,
         transitionTimingFunction: 'ease',
+
+        dragging: false,
+        lastCursorX: null,
+        lastCursorY: null,
+        delayTranslateX: null,
+        delayTranslateY: null,
+
+        // Click detection
+        startX: null,
+        startY: null,
+        clickDelta: 10,
       }
     },
 
@@ -107,8 +121,61 @@
     },
 
     methods: {
+      eventIsClick(event) {
+        const diffX = Math.abs(this.startX - event.clientX);
+        const diffY = Math.abs(this.startY - event.clientY);
+
+        return diffX < this.clickDelta && diffY < this.clickDelta;
+      },
+
       getTransition(name) {
         return `${name} ${this.transitionTime}s ${this.transitionTimingFunction}`;
+      },
+
+      mouseDown(event) {
+        this.startX = event.clientX;
+        this.startY = event.clientY;
+
+        if (!this.zoomEnabled) {
+          return;
+        }
+
+        this.dragging = true;
+        this.lastCursorX = event.clientX;
+        this.lastCursorY = event.clientY;
+      },
+
+      mouseMove(event) {
+        this.pan(event);
+      },
+
+      mouseUp(event) {
+        this.resetMouseMovement();
+
+        if (this.eventIsClick(event)) {
+          this.navigate(event);
+        }
+      },
+
+      mouseLeave(event) {
+        this.resetMouseMovement();
+      },
+
+      resetMouseMovement() {
+        this.dragging = false;
+
+        if (this.delayTranslateX !== null) {
+          this.translateX = this.delayTranslateX;
+          this.translateY = this.delayTranslateY;
+
+          this.delayTranslateX = null;
+          this.delayTranslateY = null;
+        }
+
+        this.lastCursorX = null;
+        this.lastCursorY = null;
+
+        this.transitionProperties = ['transform', 'left', 'top'];
       },
 
       navigate(event) {
@@ -182,6 +249,74 @@
         this.translateX = clickXDiff * scale;
         this.translateY = clickYDiff * scale;
         return true;
+      },
+
+      pan(event) {
+        if (!this.dragging) {
+          return;
+        }
+
+        if (this.lastCursorX === null || this.lastCursorY === null) {
+          return;
+        }
+
+        if (this.eventIsClick(event)) {
+          return;
+        }
+
+        let clickX = event.clientX;
+        let clickY = event.clientY;
+
+        let xDiff = clickX - this.lastCursorX;
+        let yDiff = clickY - this.lastCursorY;
+
+        let imageDisplayWidth = this.viewportWidth;
+        let imageDisplayHeight = this.viewportHeight;
+
+        if (this.viewportRatio > this.imageRatio) {
+          imageDisplayWidth = this.imageWidth * this.viewportHeight / this.imageHeight;
+        }
+        else {
+          imageDisplayHeight = this.imageHeight * this.viewportWidth / this.imageWidth;
+        }
+
+        let clickXDiffUnclamped = (this.translateX + xDiff) / this.scale;
+        let clickYDiffUnclamped = (this.translateY + yDiff) / this.scale;
+
+        let clickXDiffMax = clamp(
+          clickXDiffUnclamped,
+          -imageDisplayWidth / 2,
+          imageDisplayWidth / 2
+        );
+        let clickYDiffMax = clamp(
+          clickYDiffUnclamped,
+          -imageDisplayHeight / 2,
+          imageDisplayHeight / 2
+        );
+
+        let clickXDiff = clamp(
+          clickXDiffUnclamped,
+          0.5 / this.scale * this.viewportWidth - 0.5 * imageDisplayWidth,
+          -0.5 / this.scale * this.viewportWidth + 0.5 * imageDisplayWidth
+        );
+        let clickYDiff = clamp(
+          clickYDiffUnclamped,
+          0.5 / this.scale * this.viewportHeight - 0.5 * imageDisplayHeight,
+          -0.5 / this.scale * this.viewportHeight + 0.5 * imageDisplayHeight
+        );
+
+        this.transitionProperties = ['transform'];
+
+        this.translateX = clickXDiffMax * this.scale;
+        this.translateY = clickYDiffMax * this.scale;
+
+        this.delayTranslateX = clickXDiff * this.scale;
+        this.delayTranslateY = clickYDiff * this.scale;
+
+        this.lastCursorX = clickX;
+        this.lastCursorY = clickY;
+
+        return false;
       },
 
       first() {
