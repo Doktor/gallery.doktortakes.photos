6106af6112d740c3d58634b036594391ed461b3c
Doktor <doktorthehusky@gmail.com>
2021-11-09 22:42:57 -0500

    docker: Add deployment script to deploy website in production

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+error=0
+
+echo "running pre-deployment checks"
+
+if ! command -v docker &> /dev/null
+then
+  echo "error: Docker is missing"
+  error=1
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+  echo "error: Docker Compose is missing"
+  error=1
+fi
+
+if [[ ! -f ./data/config.toml ]]
+then
+  echo "error: config.toml is missing"
+  error=1
+fi
+
+if [[ ! error -eq 0 ]]
+then
+  echo "encountered errors during pre-deployment check, exiting"
+  exit 1
+else
+  echo "pre-deployment checks ran successfully"
+fi
+
+echo "starting deployment"
+
+if [[ -d ./static.1/ ]]
+then
+  echo "removing old static files"
+  rm -rf ./static.1/
+fi
+
+mkdir ./static.1/
+
+echo "building Docker images" && \
+  sudo docker-compose -f docker-compose.production.yml build && \
+  echo "starting Docker container" && \
+  sudo docker-compose -f docker-compose.production.yml up -d
+
+echo "copying Nginx config file" && \
+  sudo cp ./build/nginx/nginx.production.conf /etc/nginx/sites-enabled/doktortakes.photos.conf
+
+echo "reloading Nginx" && \
+  service nginx reload
+
+exit 0
