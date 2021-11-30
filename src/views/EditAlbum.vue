e78dcd51ad8a9d63b278b1d522008b44754a2a0d
Doktor <doktorthehusky@gmail.com>
2021-11-30 04:31:02 -0500

    ui [EditAlbum]: move album links to separate component

--- src/views/EditAlbum.vue
+++ src/views/EditAlbum.vue
@@ -5,38 +5,7 @@
     <template v-if="!loading">
       <header>
         <h2 id="album-name">{{ album.name }}</h2>
-
-        <ul class="edit-links-center">
-          <li>
-            <a
-                :href="album.url"
-                title="View album"
-                target="_blank"
-            >View</a>
-          </li>
-          <li>
-            <a
-                :href="album.admin_url"
-                title="View album on admin site"
-            >Admin</a>
-          </li>
-          <li>
-            <a
-              :href="urlProductionSite"
-              title="View album on production site"
-              target="_blank"
-              rel="noopener noreferrer"
-            >Production</a>
-          </li>
-          <li>
-            <a
-              :href="urlAlphaSite"
-              title="View album on alpha site"
-              target="_blank"
-              rel="noopener noreferrer"
-            >Alpha</a>
-          </li>
-        </ul>
+        <AlbumLinks :album="album" />
       </header>
 
       <h2>Album details</h2>
@@ -63,22 +32,21 @@
 </template>
 
 <script>
-  import AlbumCard from '@/components/albumList/AlbumCard.vue';
-  import AlbumChildren from "@/components/albumList/AlbumChildren";
-  import AlbumDetails from '@/components/editor/AlbumDetails.vue';
-  import FixedWidthContainer from "@/components/FixedWidthContainer";
-  import DeleteAlbum from '@/components/editor/DeleteAlbum.vue';
-  import PhotoManager from '@/components/editor/PhotoManager.vue';
-  import PhotoUploader from '@/components/editor/PhotoUploader.vue';
-  import {mapState} from 'vuex';
-  import {domains} from '@/store';
-  import {router} from "@/router/main";
-  import {editorTitleTemplate} from "@/store/mutations";
+import AlbumChildren from "@/components/albumList/AlbumChildren";
+import AlbumDetails from '@/components/editor/AlbumDetails.vue';
+import FixedWidthContainer from "@/components/FixedWidthContainer";
+import DeleteAlbum from '@/components/editor/DeleteAlbum.vue';
+import PhotoManager from '@/components/editor/PhotoManager.vue';
+import PhotoUploader from '@/components/editor/PhotoUploader.vue';
+import {mapState} from 'vuex';
+import {router} from "@/router/main";
+import {editorTitleTemplate} from "@/store/mutations";
+import AlbumLinks from "@/components/editor/AlbumLinks";
 
 
   export default {
     components: {
-      AlbumCard,
+      AlbumLinks,
       AlbumChildren,
       AlbumDetails,
       DeleteAlbum,
@@ -101,14 +69,6 @@
 
       routePath() {
         return this.$route.params.path;
-      },
-
-      urlAlphaSite() {
-        return new URL(this.album.url, domains.alpha).href;
-      },
-
-      urlProductionSite() {
-        return new URL(this.album.url, domains.production).href;
       },
     },
 
