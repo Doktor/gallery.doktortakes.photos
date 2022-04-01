47f1cf267b8e575ee15b0b436828a2208a8dad7f
Doktor <doktorthehusky@gmail.com>
2022-04-01 00:13:28 -0400

    #90: Add Thumbnail model
    Closes #90.

--- photos/models/__init__.py
+++ photos/models/__init__.py
@@ -1,6 +1,6 @@
 from .album import Album
 from .file import File
 from .hero_photo import HeroPhoto
-from .photo import Photo
+from .photo import Photo, Thumbnail
 from .tag import Tag
 from .tagline import Tagline
---
+++ photos/models/photo/thumbnail.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+THUMBNAIL_DISPLAY = 'display'
+THUMBNAIL_COVER = 'cover'
+THUMBNAIL_SMALL_SQUARE = 'square'
+
+THUMBNAIL_TYPES = (
+    (THUMBNAIL_DISPLAY, 'Display'),
+    (THUMBNAIL_COVER, 'Cover'),
+    (THUMBNAIL_SMALL_SQUARE, 'Square'),
+)
+
+
+def get_filename(thumbnail: 'Thumbnail', filename: str) -> str:
+    photo = thumbnail.photo
+    datetime = photo.taken.strftime("%Y%m%d_%H%M%S")
+    dimensions = f"{photo.width}x{photo.height}"
+
+    return f"images/{datetime}_{photo.short_md5}_{dimensions}.jpg"
+
+
+class Thumbnail(models.Model):
+    photo = models.ForeignKey("Photo", on_delete=models.CASCADE, related_name='thumbnails')
+    type = models.CharField(max_length=10, choices=THUMBNAIL_TYPES, blank=True)
+
+    # Image
+
+    image = models.ImageField(
+        upload_to=get_filename,
+        blank=True, null=True, editable=False)
+    is_watermarked = models.BooleanField(default=False)
+
+    width = models.PositiveIntegerField(default=0, editable=False)
+    height = models.PositiveIntegerField(default=0, editable=False)
+    file_size = models.CharField(max_length=50, editable=False, blank=True)
+
+    # Bookkeeping
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Thumbnail for photo {self.photo.id} ({self.width}x{self.height})'
