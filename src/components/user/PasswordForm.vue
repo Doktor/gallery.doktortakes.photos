d73c36b5189ceca7a7d8b4a676e57fd7bf97d3c1
Doktor <doktorthehusky@gmail.com>
2022-04-03 01:28:15 -0400

    refactor: Reformat "src/components/user/PasswordForm.vue"

--- src/components/user/PasswordForm.vue
+++ src/components/user/PasswordForm.vue
@@ -2,40 +2,40 @@
   <form class="form--1-column form--small">
     <fieldset>
       <PasswordFormField
-          :name="'Current'"
-          v-model="current.value"
-          @input="onEdit('current')"
-          :errors="current.errors"
+        :name="'Current'"
+        v-model="current.value"
+        @input="onEdit('current')"
+        :errors="current.errors"
       />
 
       <PasswordFormField
-          :name="'New password'"
-          v-model="password1.value"
-          @input="onEdit('password1')"
-          :errors="password1.errors"
-          :help-text="'Your password must contain at least 8 characters.'"
+        :name="'New password'"
+        v-model="password1.value"
+        @input="onEdit('password1')"
+        :errors="password1.errors"
+        :help-text="'Your password must contain at least 8 characters.'"
       />
 
       <PasswordFormField
-          :name="'Repeat'"
-          v-model="password2.value"
-          @input="onEdit('password2')"
-          :errors="password2.errors"
+        :name="'Repeat'"
+        v-model="password2.value"
+        @input="onEdit('password2')"
+        :errors="password2.errors"
       />
     </fieldset>
 
     <div class="form-buttons">
       <router-link
-          class="form-button form-button-secondary"
-          :to="{name: 'user'}"
+        class="form-button form-button-secondary"
+        :to="{ name: 'user' }"
       >
         Cancel
       </router-link>
 
       <button
-          class="form-button form-button-primary"
-          type="submit"
-          @click.prevent="submit"
+        class="form-button form-button-primary"
+        type="submit"
+        @click.prevent="submit"
       >
         Save
       </button>
@@ -44,117 +44,121 @@
 </template>
 
 <script>
-  import PasswordFormField from "./PasswordFormField";
+import PasswordFormField from "./PasswordFormField";
 
+const errors = {
+  empty: "This field can't be empty.",
+  same: "Your new password can't be the same as your current password.",
+  tooShort: "The new password is too short.",
+  noMatch: "The new passwords don't match.",
+};
 
-  const errors = {
-    empty: "This field can't be empty.",
-    same: "Your new password can't be the same as your current password.",
-    tooShort: "The new password is too short.",
-    noMatch: "The new passwords don't match."
-  };
+export default {
+  components: {
+    PasswordFormField,
+  },
 
+  data() {
+    return {
+      current: {
+        edited: false,
+        maxTyped: 0,
 
-  export default {
-    components: {
-      PasswordFormField,
+        errors: [],
+        value: "",
+      },
+      password1: {
+        edited: false,
+        maxTyped: 0,
+
+        errors: [],
+        value: "",
+      },
+      password2: {
+        edited: false,
+        maxTyped: 0,
+
+        errors: [],
+        value: "",
+      },
+    };
+  },
+
+  methods: {
+    validate(force = false) {
+      this.current.errors = [];
+      this.password1.errors = [];
+      this.password2.errors = [];
+
+      // Empty fields
+      if ((force || this.current.edited) && !this.current.value) {
+        this.current.errors.push(errors.empty);
+      }
+      if ((force || this.password1.edited) && !this.password1.value) {
+        this.password1.errors.push(errors.empty);
+      }
+      if ((force || this.password2.edited) && !this.password2.value) {
+        this.password2.errors.push(errors.empty);
+      }
+
+      if (this.password1.value) {
+        // New password is same as the old password
+        if (this.current.value === this.password1.value) {
+          this.password1.errors.push(errors.same);
+        }
+
+        // New password is too short
+        if (
+          this.password1.value.length < 8 &&
+          (force || this.password1.maxTyped >= 8)
+        ) {
+          this.password1.errors.push(errors.tooShort);
+        }
+
+        // New passwords don't match
+        if (
+          this.password2.value &&
+          this.password1.value !== this.password2.value
+        ) {
+          this.password2.errors.push(errors.noMatch);
+        }
+      }
+
+      return (
+        this.current.errors.length === 0 &&
+        this.password1.errors.length === 0 &&
+        this.password2.errors.length === 0
+      );
     },
 
-    data() {
-      return {
-        current: {
-          edited: false,
-          maxTyped: 0,
+    onEdit(fieldName) {
+      let field = this[fieldName];
+      field.edited = true;
+      field.maxLength = Math.max(field.maxTyped, field.value.length);
 
-          errors: [],
-          value: "",
-        },
-        password1: {
-          edited: false,
-          maxTyped: 0,
+      this.validate();
+    },
 
-          errors: [],
-          value: "",
-        },
-        password2: {
-          edited: false,
-          maxTyped: 0,
-
-          errors: [],
-          value: "",
-        },
+    submit() {
+      if (this.validate(true)) {
+        this.$store.dispatch("changePassword", {
+          current: this.current.value,
+          password1: this.password1.value,
+          password2: this.password2.value,
+        });
       }
     },
-
-    methods: {
-      validate(force = false) {
-        this.current.errors = [];
-        this.password1.errors = [];
-        this.password2.errors = [];
-
-        // Empty fields
-        if ((force || this.current.edited) && !this.current.value) {
-          this.current.errors.push(errors.empty);
-        }
-        if ((force || this.password1.edited) && !this.password1.value) {
-          this.password1.errors.push(errors.empty);
-        }
-        if ((force || this.password2.edited) && !this.password2.value) {
-          this.password2.errors.push(errors.empty);
-        }
-
-        if (this.password1.value) {
-          // New password is same as the old password
-          if (this.current.value === this.password1.value) {
-            this.password1.errors.push(errors.same);
-          }
-
-          // New password is too short
-          if (this.password1.value.length < 8
-              && (force || this.password1.maxTyped >= 8)) {
-            this.password1.errors.push(errors.tooShort);
-          }
-
-          // New passwords don't match
-          if (this.password2.value
-              && this.password1.value !== this.password2.value) {
-            this.password2.errors.push(errors.noMatch);
-          }
-        }
-
-        return this.current.errors.length === 0
-            && this.password1.errors.length === 0
-            && this.password2.errors.length === 0;
-      },
-
-      onEdit(fieldName) {
-        let field = this[fieldName];
-        field.edited = true;
-        field.maxLength = Math.max(field.maxTyped, field.value.length);
-
-        this.validate();
-      },
-
-      submit() {
-        if (this.validate(true)) {
-          this.$store.dispatch('changePassword', {
-            current: this.current.value,
-            password1: this.password1.value,
-            password2: this.password2.value,
-          });
-        }
-      },
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  form {
-    width: 50%;
-    margin: 0 auto;
-  }
+form {
+  width: 50%;
+  margin: 0 auto;
+}
 
-  .form-buttons {
-    justify-content: space-between;
-  }
+.form-buttons {
+  justify-content: space-between;
+}
 </style>
