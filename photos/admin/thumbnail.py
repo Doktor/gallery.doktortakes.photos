f8db98dc607357cb0d9b0a2a50b0f52cfd115b54
Doktor <doktorthehusky@gmail.com>
2022-09-16 04:56:10 -0400

    api [ThumbnailAdmin]: add "type" to "list_display"

--- photos/admin/thumbnail.py
+++ photos/admin/thumbnail.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 class ThumbnailAdmin(admin.ModelAdmin):
     fields = ('type', 'image', 'is_watermarked', 'width', 'height', 'file_size', 'created_date', 'updated_date')
     readonly_fields = ('image', 'is_watermarked', 'width', 'height', 'file_size', 'created_date', 'updated_date')
-    list_display = ('pk', 'photo_id', 'image', 'width', 'height', 'file_size', 'created_date', 'updated_date')
+    list_display = ('pk', 'photo_id', 'image', 'type', 'width', 'height', 'file_size', 'created_date', 'updated_date')
