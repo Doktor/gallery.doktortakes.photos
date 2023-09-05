b6eb7bf06f300df816dccb021d4fab4c8ae95914
Doktor <doktorthehusky@gmail.com>
2023-09-05 04:42:37 -0400

    api [TaxonPhotoList]: Add option to get photos from all descendants

--- photos/api/views/taxon.py
+++ photos/api/views/taxon.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from photos.models import Taxon
+from photos.models import Taxon, PhotoTaxon, Photo
 from photos.api.serializers import TaxonSerializer, PhotoSerializer
 
 
@@ -20,7 +20,24 @@ class TaxonPhotoList(APIView):
     @staticmethod
     def get(request: Request, catalog_id: str) -> Response:
         taxon = get_object_or_404(Taxon, catalog_id=catalog_id)
-        photos = taxon.photos.all()
-        serializer = PhotoSerializer(photos, many=True)
 
+        # TODO: replace this with something more efficient
+        if request.GET.get('recursive', False):
+            taxa = [taxon]
+            queue = [taxon]
+
+            while queue:
+                top = queue.pop()
+
+                children = top.children.all()
+                taxa.extend(children)
+                queue.extend(children)
+
+            links = PhotoTaxon.objects.filter(taxon__in=taxa)
+            photo_ids = set(link.photo.id for link in links)
+            photos = Photo.objects.filter(id__in=photo_ids)
+        else:
+            photos = taxon.photos.all()
+
+        serializer = PhotoSerializer(photos, many=True)
         return Response(serializer.data)
