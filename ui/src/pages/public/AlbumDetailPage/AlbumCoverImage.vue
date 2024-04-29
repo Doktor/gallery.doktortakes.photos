999e60133acd9abbe0c73372af936c3f947d8b5c
Doktor <doktorthehusky@gmail.com>
2024-04-28 21:38:36 -0400

    ui [AlbumCoverImage]: Restyle component

--- ui/src/pages/public/AlbumDetailPage/AlbumCoverImage.vue
+++ ui/src/pages/public/AlbumDetailPage/AlbumCoverImage.vue
@@ -1,10 +1,5 @@
 <template>
-  <img
-    class="album-cover-image"
-    alt="Cover photo"
-    :src="album.cover.thumbnail.url"
-    :title="album.name"
-  />
+  <div class="album-cover-image" :style="styles"></div>
 </template>
 
 <script>
@@ -15,20 +10,36 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    styles() {
+      return {
+        backgroundImage: `url(${this.album.cover.thumbnail.url})`,
+      };
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .album-cover-image {
-  position: absolute;
-  z-index: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  background: no-repeat 50% math.div(100%, 3);
+  background-size: cover;
 
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  &,
+  &::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  &::after {
+    content: "";
+
+    backdrop-filter: blur(8px);
+    pointer-events: none;
+  }
 }
 </style>
