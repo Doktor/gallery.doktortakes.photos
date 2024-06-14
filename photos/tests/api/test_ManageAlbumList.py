ab40ffa6e6a812074cc5ae23000889fb9940241d
Doktor <doktorthehusky@gmail.com>
2024-06-14 14:53:51 -0400

    tests (api): Update tests for ManageAlbumList

--- photos/tests/api/test_ManageAlbumList.py
+++ photos/tests/api/test_ManageAlbumList.py
@@ -2,19 +2,23 @@ import pytest
 from http import HTTPStatus as Status
 
 from django.urls import reverse
-from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory, force_authenticate
 
+from photos.api.views.manage import ManageAlbumList
 from photos.tests.api.album import AlbumFactory
-from photos.tests.api.utils import create_user, Level
+from photos.tests.api.utils import LicenseFactory, create_superuser, create_user, Level
 
 
-URL = reverse('api_manage_albums')
+url = reverse('api_manage_albums')
 
 
 @pytest.mark.django_db
 class TestManageAlbumList:
-    def setup_method(self):
-        self.client = APIClient()
+    @classmethod
+    @pytest.fixture(autouse=True)
+    def setup_class(cls):
+        cls.factory = APIRequestFactory()
+        cls.default_license = LicenseFactory()
 
     @pytest.mark.parametrize("level", [
         Level.ANONYMOUS,
@@ -22,95 +26,129 @@ class TestManageAlbumList:
     ])
     def test_post__no_permission__forbidden(self, level: Level):
         # Arrange
-        user = create_user(level)
-        self.client.force_authenticate(user=user)
-
         body = {
-            'name': 'Album Name 1',
+            # Required fields
+            'groups': [],
+            'name': 'Album Name',
+            'license_id': self.default_license.id,
             'start': '2020-01-01',
-            'end': '2020-01-31',
+            'tags': [],
+            'users': [],
         }
+        request = self.factory.post(url, data=body)
+        force_authenticate(request, user=create_user(level))
 
         # Act
-        response = self.client.post(URL, data=body)
+        response = ManageAlbumList.as_view()(request)
 
         # Assert
         assert response.status_code == Status.FORBIDDEN
 
-    def test_post__top_level_album_name_unavailable__bad_request(self):
+    def test_post__top_level_album_name_already_in_use__bad_request(self):
         # Arrange
-        user = create_user(Level.SUPERUSER)
-        self.client.force_authenticate(user=user)
-
-        album = AlbumFactory(name='Album Name 1', parent=None)
+        album_name = 'Album Name'
+        AlbumFactory(name=album_name, parent=None)
 
         body = {
-            'name': album.name,
+            'name': album_name,
+
+            # Required fields
+            'groups': [],
+            'license_id': self.default_license.id,
             'parent': None,
             'start': '2020-01-01',
-
             'tags': [],
             'users': [],
-            'groups': [],
         }
+        request = self.factory.post(url, data=body)
+        force_authenticate(request, user=create_superuser())
 
         # Act
-        response = self.client.post(URL, data=body)
+        response = ManageAlbumList.as_view()(request)
 
         # Assert
         assert response.status_code == Status.BAD_REQUEST
+        assert response.data == {
+            'non_field_errors': ['A top-level album with that name already exists.']}
 
-    def test_post__album_name_unavailable__bad_request(self):
+    def test_post__child_album_name_already_in_use__bad_request(self):
         # Arrange
-        user = create_user(Level.SUPERUSER)
-        self.client.force_authenticate(user=user)
+        parent_album = AlbumFactory(name='Parent Album Name', parent=None)
 
-        parent = AlbumFactory(name='Parent Album', parent=None)
-        album = AlbumFactory(name='Album Name 1', parent=parent)
+        child_album_name = 'Child Album Name'
+        AlbumFactory(name=child_album_name, parent=parent_album)
 
         body = {
-            'name': album.name,
-            'parent': parent.path,
-            'start': '2020-01-01',
+            'name': child_album_name,
+            'parent': parent_album.path,
 
+            # Required fields
+            'groups': [],
+            'license_id': self.default_license.id,
+            'start': '2020-01-01',
             'tags': [],
             'users': [],
-            'groups': [],
         }
 
+        request = self.factory.post(url, data=body)
+        force_authenticate(request, user=create_superuser())
+
         # Act
-        response = self.client.post(URL, data=body)
+        response = ManageAlbumList.as_view()(request)
 
         # Assert
         assert response.status_code == Status.BAD_REQUEST
+        assert response.data == {
+            'non_field_errors': ['An album with that name and parent album already exists.']}
 
-    @pytest.mark.parametrize("level", [
-        Level.STAFF,
-        Level.SUPERUSER,
-    ])
-    def test_post__created(self, level: Level):
+    def test_post__top_level_album__created(self):
         # Arrange
-        user = create_user(level)
-        self.client.force_authenticate(user=user)
-
         body = {
-            'name': 'Album Name 1',
-            'start': '2020-01-01',
-            'end': '2020-01-31',
-            'parent': None,
+            'name': 'Album Name',
 
+            # Required fields
+            'groups': [],
+            'license_id': self.default_license.id,
+            'parent': None,
+            'start': '2020-01-01',
             'tags': [],
             'users': [],
-            'groups': [],
         }
+        request = self.factory.post(url, data=body)
+        force_authenticate(request, user=create_superuser())
 
         # Act
-        response = self.client.post(URL, data=body)
+        response = ManageAlbumList.as_view()(request)
 
         # Assert
         assert response.status_code == Status.CREATED
 
-        actual = response.data
-        assert actual['name'] == 'Album Name 1'
-        assert actual['start'] == '2020-01-01'
-        assert actual['end'] == '2020-01-31'
+        actual_album = response.data
+        assert actual_album['name'] == 'Album Name'
+
+    def test_post__child_album__created(self):
+        # Arrange
+        parent_album = AlbumFactory(name='Parent Album Name')
+
+        body = {
+            'name': 'Child Album Name',
+            'parent': parent_album.path,
+
+            # Required fields
+            'groups': [],
+            'license_id': self.default_license.id,
+            'start': '2024-01-01',
+            'tags': [],
+            'users': [],
+        }
+        request = self.factory.post(url, data=body)
+        force_authenticate(request, user=create_superuser())
+
+        # Act
+        response = ManageAlbumList.as_view()(request)
+
+        # Assert
+        assert response.status_code == Status.CREATED
+
+        actual_album = response.data
+        assert actual_album['name'] == 'Child Album Name'
