f931eed99cf165a869d39c6577dd8bfb587db265
Doktor <doktorbarks@gmail.com>
2026-03-14 13:12:23 -0400

    ui: Update sidebar logo

--- ui/src/components/sidebar/SidebarHeader.vue
+++ ui/src/components/sidebar/SidebarHeader.vue
@@ -3,21 +3,65 @@
     <h1 class="sidebar-logo">
       <router-link
         class="sidebar-logo-link"
-        title="Doktor Takes Photos"
+        :title="title"
+        @click="onClick"
         :to="{ name: 'index' }"
       >
-        <div>Doktor</div>
-        <div>Takes</div>
-        <div>Photos</div>
+        <div v-for="associate in associates" class="logo-name">
+          <span :style="`color: ${associate.color}`">{{ associate.name }}</span>
+          <span class="logo-gray-text">+</span>
+        </div>
+
+        <div class="logo-subtitle">
+          <div class="logo-gray-text logo-small-text">Associates</div>
+          <div class="logo-gray-text logo-small-text">GmbH</div>
+        </div>
+
+        <div class="logo-bar">
+          <div
+            v-for="associate in associates"
+            :style="`background-color: ${associate.color} `"
+          />
+        </div>
       </router-link>
     </h1>
   </header>
 </template>
 
-<script>
-export default {
-  name: "SidebarHeader",
-};
+<script setup>
+import { shuffleArray } from "@/utils";
+import { computed, reactive } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const associates = reactive([
+  {
+    name: "Doktor",
+    color: "#0078ff",
+  },
+  {
+    name: "Archer",
+    color: "#ff0000",
+  },
+  {
+    name: "Baxter",
+    color: "#ff9900",
+  },
+]);
+
+const title = computed(() => {
+  return (
+    associates.map((associate) => associate.name).join(" + ") +
+    " Associates GmbH"
+  );
+});
+
+function onClick() {
+  if (route.name === "index") {
+    shuffleArray(associates);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -27,19 +71,45 @@ export default {
 
 .sidebar-logo {
   @include variables.logo-font();
+  text-align: left;
 
   margin: 0;
   padding: 0;
-  padding-bottom: 8px;
-  border-bottom: 8px solid variables.$text-blue;
 
-  @media (width >= variables.$full-layout-breakpoint + 1) {
-    font-size: 3.5rem;
-  }
+  user-select: none;
 }
 
 .sidebar-logo-link {
-  color: variables.$text-blue;
   text-decoration: none;
+}
+
+.logo-name {
+  display: flex;
+  justify-content: space-between;
+
+  @media (width >= variables.$full-layout-breakpoint + 1) {
+    font-size: 48px;
+  }
+}
+
+.logo-subtitle {
+  margin-top: 4px;
+}
+
+.logo-gray-text {
+  color: #333333;
+}
+
+.logo-small-text {
+  font-size: 36px;
+  margin-bottom: 8px;
+}
+
+.logo-bar {
+  margin-top: 8px;
+
+  div {
+    height: 12px;
+  }
 }
 </style>
